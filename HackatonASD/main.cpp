18487cccad8d3a7283483eaafbd57829cee058f7
Andrei Maga <andreimariomaga@gmail.com>
2020-06-01 20:20:41 +0300

    Added theme

--- HackatonASD/main.cpp
+++ HackatonASD/main.cpp
@@ -1,9 +1,17 @@
 #include "ui/HackatonASD.h"
 #include <QtWidgets/QApplication>
+#include <QFile>
+#include <QTextStream>
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
+
+    QFile file(":/dark.qss");
+    file.open(QFile::ReadOnly | QFile::Text);
+    QTextStream stream(&file);
+    a.setStyleSheet(stream.readAll());
+
     HackatonASD w;
     w.show();
     return a.exec();
